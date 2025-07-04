99b57b5d956d21fa1e7f9583bed6581ab7ab1884
Rachel Saluna <mariarachelms@gmail.com>
2025-07-04 16:44:58 +0800

    Fix: Add node-fetch for API call
    # Conflicts:
    #	friends-quiz-backend/.env

--- friends-quiz-backend/server.js
+++ friends-quiz-backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fetch = require('node-fetch');
 const cors = require('cors');
+
 require('dotenv').config();
 
 const app = express();
